b1aede7c7f8e426cdb942c9bff32716f8e1aef50
MD ISMAIL <mdismail85773@gmail.com>
2024-09-17 23:26:19 +0800

    assignment

---
+++ myapp/forms.py
@@ -0,0 +1,89 @@
+from django import forms
+from .models import Employee
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['name', 'address', 'phone_number', 'salary', 'designation', 'description']
+        
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-input mt-1 block w-full h-12 pl-4 pr-4 rounded-lg border border-gray-300',  
+                'placeholder': 'Enter full name',
+            }),
+            'address': forms.TextInput(attrs={
+                'class': 'form-input mt-1 block w-full h-12 pl-4 pr-4 rounded-lg border border-gray-300',  
+                'placeholder': 'Enter address',
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'class': 'form-input mt-1 block w-full h-12 pl-4 pr-4 rounded-lg border border-gray-300',  
+                'placeholder': 'Enter phone number',
+            }),
+            'salary': forms.NumberInput(attrs={
+                'class': 'form-input mt-1 block w-full h-12 pl-4 pr-4 rounded-lg border border-gray-300',
+                'placeholder': '$ Enter salary',
+            }),
+            'designation': forms.Select(attrs={
+                'class': 'border-gray-300 rounded-md p-2 w-full',
+            }),
+
+            'description': forms.Textarea(attrs={
+                'class': 'form-input mt-1 block w-full h-12 pl-4 pr-4 rounded-lg border border-gray-300',
+                'placeholder': 'Enter a brief profile summary',
+                'rows': 1,
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        is_update = kwargs.pop('is_update', False)
+        super(EmployeeForm, self).__init__(*args, **kwargs)
+
+        if is_update:
+            self.fields['salary'].widget.attrs['readonly'] = True
+            self.fields['designation'].disabled = True
+
+    def clean_designation(self):
+        if self.fields['designation'].disabled:
+            return self.initial['designation']
+        
+        return self.cleaned_data.get('designation')
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-base py-2 px-3',
+                'placeholder': 'Enter your username'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-base py-2 px-3',
+                'placeholder': 'Enter your email'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-base py-2 px-3',
+                'placeholder': 'Enter your password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-base py-2 px-3',
+                'placeholder': 'Confirm your password'
+            }),
+        }
+
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg py-2 px-3',
+        'placeholder': 'Enter your username',
+        'autofocus': True
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-input mt-2 block w-full bg-white border border-gray-300 rounded-lg py-2 px-3',
+        'placeholder': 'Enter your password',
+    }))
---
+++ myapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from myapp import views
+
+
+
+urlpatterns = [
+    path('home/', views.index, name='home'),
+    path('add-employee', views.add_employee, name='add_employee'),
+    path('details/<int:pk>', views.details, name='details'),
+    path('update/<int:pk>', views.update, name='update'),
+    path('management/', views.management, name='management'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('register/', views.register, name='register'),
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile, name='profile'),
+]   
